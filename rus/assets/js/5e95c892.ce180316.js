ded4b62fe388a020037d8cc5a73205d2be2af257
Anatoliy Vasilkov <xaylpq@gmail.com>
2024-12-08 02:19:44 +0300

    Deploy app to GitHub Pages

---
+++ rus/assets/js/5e95c892.ce180316.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoka=self.webpackChunkdoka||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(6540);var a=r(4164),u=r(1213),c=r(7559),d=r(2831),n=r(781),t=r(4848);function k(e){return(0,t.jsx)(u.e3,{className:(0,a.A)(c.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,d.v)(e.route.routes)})})}}}]);
---
+++ rus/assets/js/b363fbd9.9742b858.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoka=self.webpackChunkdoka||[]).push([[6830],{6676:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/demo_doka/rus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
